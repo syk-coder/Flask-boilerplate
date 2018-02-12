5b2eb5446b9fb736a8397ce705e39cc228d63ef7
Raghavender Mittapalli <raghavender.m@flipkart.com>
2018-02-12 11:00:43 +0530

    added code for post and indexing using elastic search

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
+import os
+
 import connexion
 
 from connexion.resolver import RestyResolver
 from flask_injector import FlaskInjector
 from injector import Binder
 
+from configuration.es_mapper import room_mapping
+from services.elasticsearch import ElasticSearchIndex, ElasticSearchFactory
 from services.provider import ItemsProvider
 
 
 def configure(binder: Binder) -> Binder:
     binder.bind(
         ItemsProvider,
-        ItemsProvider([{"Name": "Test1"}])
+        ItemsProvider([{"Name": "Test1"}]),
+        ElasticSearchIndex,
+        ElasticSearchIndex(
+            ElasticSearchFactory(
+                os.environ['ELASTICSEARCH_HOST'],
+                os.environ['ELASTICSEARCH_PORT']
+            ),
+            'rooms',
+            'room',
+            room_mapping
+        )
     )
+
+    return binder
 
 
 if __name__ == '__main__':
